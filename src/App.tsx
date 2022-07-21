3302e67ff5a43f89d1764f229e6e8a887b5050e6
rodrigomsrocha <rodrigomtc82@gmail.com>
2022-07-21 03:32:20 -0300

    Estrutura da aplicação - Configurando temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/theme/default";
 
 export function App() {
   return (
-    <div>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="success" />
       <Button variant="danger" />
-    </div>
+    </ThemeProvider>
   );
 }
