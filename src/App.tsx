b8907d964b1d20e09b741268401c79a5260391d1
rodrigomsrocha <rodrigomtc82@gmail.com>
2022-07-21 04:18:31 -0300

    Estrutura da aplicação - Configurando ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
-import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/theme/default";
+import { ThemeProvider } from 'styled-components';
+import { Button } from './components/Button';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/theme/default';
 
 export function App() {
   return (
--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,16 +1,16 @@
-import styled, { css } from "styled-components";
+import styled, { css } from 'styled-components';
 
-export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
 
 interface ButtonContainerProps {
   variant: ButtonVariant;
 }
 
 const buttonVariants = {
-  primary: "purple",
-  secondary: "orange",
-  success: "green",
-  danger: "red",
+  primary: 'purple',
+  secondary: 'orange',
+  success: 'green',
+  danger: 'red',
 };
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
@@ -20,7 +20,7 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   border: 0;
   margin: 8px;
 
-  background: ${(props) => props.theme["green-500"]};
+  background: ${(props) => props.theme['green-500']};
   color: ${(props) => props.theme.white};
 
   /* ${(props) => {
