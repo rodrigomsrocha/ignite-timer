73af77f6e5cccfe3514d5f6890975f0e37d3c8b7
rodrigomsrocha <rodrigomtc82@gmail.com>
2022-07-21 03:19:33 -0300

    Estrutura da aplicação - Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/Button";
+
 export function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
     </div>
   );
 }
