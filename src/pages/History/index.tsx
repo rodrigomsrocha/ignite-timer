efb2d2b8f8dd604ca0ddbf78b434ce06dcc101be
rodrigomsrocha <rodrigomtc82@gmail.com>
2022-07-24 01:44:42 -0300

    Contexto no React - Formatação de data

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,5 @@
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import { useCyclesContext } from '../../contexts/CyclesContext'
 import { HistoryContainer, HistoryList, Status } from './styles'
 
@@ -22,7 +24,12 @@ export function History() {
               <tr key={cycle.id}>
                 <td>{cycle.task}</td>
                 <td>{cycle.minutesAmount} minutos</td>
-                <td>{cycle.startDate.toISOString()}</td>
+                <td>
+                  {formatDistanceToNow(cycle.startDate, {
+                    addSuffix: true,
+                    locale: ptBR,
+                  })}
+                </td>
                 <td>
                   {cycle.finishedDate && (
                     <Status statusColor="green">Concluído</Status>
